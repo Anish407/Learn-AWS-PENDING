6dc558e306ca4bc3c7ebaa6b44143ff493444091
Anish <anish.arvind407@gmail.com>
2024-06-07 11:04:48 +0200

    sns publisher added

--- SnsPublisher.Api/Program.cs
+++ SnsPublisher.Api/Program.cs
@@ -1,3 +1,9 @@
+using System.Text.Json;
+using Amazon.SimpleNotificationService;
+using Amazon.SimpleNotificationService.Model;
+using Microsoft.AspNetCore.Http.HttpResults;
+using SNS.Common.Events;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -16,27 +22,63 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
+app.MapPost("/PublishToSNS", PublishMessageHandler)
+    .WithName("PublishToSNS")
     .WithOpenApi();
 
 app.Run();
+
+static async Task<Results<Ok<string>, BadRequest<string>>> PublishMessageHandler(
+    IAmazonSimpleNotificationService snsClient)
+{
+    try
+    {
+        string topicName = "Customers";
+        string topicArn = await GetTopicArn(snsClient, topicName);
+
+        var messageRequest = GetMessageRequest(topicArn);
+        await snsClient.PublishAsync(messageRequest);
+        return TypedResults.Ok("Message sent");
+    }
+    catch (Exception e)
+    {
+        return TypedResults.BadRequest(e.Message);
+    }
+};
+
+
+static async Task<string> GetTopicArn(IAmazonSimpleNotificationService amazonSimpleNotificationService, string s)
+{
+    var topicResponse = await amazonSimpleNotificationService.FindTopicAsync(s);
+    return topicResponse.TopicArn;
+}
+
+static PublishRequest GetMessageRequest(string s)
+{
+    var randomNumber = new Random().Next();
+    OrderCreatedEvent @event =
+        new OrderCreatedEvent(Id: randomNumber,
+            $"OrderId:{Guid.NewGuid()}",
+            Date: DateTime.Now,
+            $"Anish:{randomNumber}");
+
+    return new PublishRequest()
+    {
+        TopicArn = s,
+        Message = JsonSerializer.Serialize(@event),
+        MessageAttributes = new Dictionary<string, MessageAttributeValue>
+        {
+            {
+                "MessageType", new MessageAttributeValue()
+                {
+                    DataType = "String",
+                    StringValue = nameof(OrderCreatedEvent)
+                }
+            }
+        }
+    };
+}
+
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
