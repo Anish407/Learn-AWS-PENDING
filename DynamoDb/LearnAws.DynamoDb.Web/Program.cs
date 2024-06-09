8ea3df0e43319d72b2e6711d38a119a437daf5d4
Anish <anish.arvind407@gmail.com>
2024-06-09 09:21:17 +0200

    rearange

--- DynamoDb/LearnAws.DynamoDb.Web/Program.cs
+++ DynamoDb/LearnAws.DynamoDb.Web/Program.cs
@@ -3,6 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
 var app = builder.Build();
 builder.AddCustomerServices();
 
